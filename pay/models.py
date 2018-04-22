d7b9c8ee0b1605fe170d0a808d0f57aeb9ddeebc
Mathew George <mac@Mathews-MacBook-Air.local>
2018-04-22 12:17:29 +0530

    Change in Broker model

--- pay/models.py
+++ pay/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class Broker(models.Model):
-    email = models.CharField(max_length=50)
-    name = models.CharField(max_length=30)
-    pan_num = models.CharField(max_length=15)
-    bank_acc_num = models.CharField(max_length=30)
-    ifsc = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-
-
 class Owner(models.Model):
     email = models.CharField(max_length=50)
     name = models.CharField(max_length=30)
@@ -29,6 +18,18 @@ class Tenant(models.Model):
     pan_num = models.CharField(max_length=15)
     bank_acc_num = models.CharField(max_length=30)
     ifsc = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Broker(models.Model):
+    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
+    pan_num = models.CharField(max_length=15)
+    bank_acc_num = models.CharField(max_length=30)
+    ifsc = models.CharField(max_length=30)
+    owners = models.ManyToManyField(Owner, blank=True)
+    tenants = models.ManyToManyField(Tenant, blank=True)
 
     def __str__(self):
         return self.name
