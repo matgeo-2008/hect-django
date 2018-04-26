ff72084e007880e36cc88d0cfcd97a8f0b6f4272
Mathew George <mac@Mathews-MacBook-Air.local>
2018-04-26 19:40:41 +0530

    Authentication v1

--- pay/api/urls.py
+++ pay/api/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import BrokerAPIView
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'pay'
 
 urlpatterns = [
     url(r'^broker/', BrokerAPIView.as_view(), name='broker-detail'),
+    url(r'^token/$', TokenObtainPairView.as_view()),
+    url(r'^token/refresh/$', TokenRefreshView.as_view()),
 ]
--- pay/api/views.py
+++ pay/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from pay.models import Broker, Owner, Tenant
-from .serializers import BrokerSerializer
+from .serializers import BrokerSerializer, TenantSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -13,3 +14,11 @@ class BrokerAPIView(APIView):
         broker = get_object_or_404(Broker, pk=query)
         serializer = BrokerSerializer(broker, many=False)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TenantSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
